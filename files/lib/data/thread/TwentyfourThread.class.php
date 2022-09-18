d8049cf21d29550d21f937ac986ba80758c696b6
Sascha Greuel <github@1-2.dev>
2022-09-18 22:46:57 +0200

    5.5.1

--- files/lib/data/thread/TwentyfourThread.class.php
+++ files/lib/data/thread/TwentyfourThread.class.php
@@ -1,5 +1,27 @@
 <?php
+
+/*
+ * Copyright by Udo Zaydowicz.
+ * Modified by SoftCreatR.dev.
+ *
+ * License: http://opensource.org/licenses/lgpl-license.php
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 namespace wbb\data\thread;
+
 use wbb\data\board\Board;
 use wbb\data\board\BoardCache;
 use wcf\data\DatabaseObjectDecorator;
@@ -9,65 +31,67 @@ use wcf\system\WCF;
 
 /**
  * Represents an 24h thread.
- * 
- * @author		2017-2022 Zaydowicz
- * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @package		com.uz.wbb.twentyfourThreads
  */
-class TwentyfourThread extends DatabaseObjectDecorator {
-	/**
-	 * @inheritDoc
-	 */
-	protected static $baseClass = Thread::class;
-	
-	/**
-	 * number of 24h threads
-	 */
-	protected static $twentyfourThreads;
-	
-	/**
-	 * Returns the number of 24h threads.
-	 */
-	public static function getTwentyfourThreads() {
-		if (self::$twentyfourThreads === null) {
-			self::$twentyfourThreads = 0;
-			
-			$boardIDs = Board::getAccessibleBoardIDs();
-			// removed ignored boards
-			foreach ($boardIDs as $key => $boardID) {
-				$board = BoardCache::getInstance()->getBoard($boardID);
-				if ($board->isIgnored()) {
-					unset($boardIDs[$key]);
-				}
-			}
-			
-			if (!empty($boardIDs)) {
-				$conditionBuilder = new PreparedStatementConditionBuilder();
-				$conditionBuilder->add("thread.boardID IN (?)", [$boardIDs]);
-				$conditionBuilder->add("thread.isDeleted = 0 AND thread.isDisabled = 0 AND thread.movedThreadID IS NULL");
-				
-				// apply options
-				if (!WBB_BOARD_THREAD_TWENTYFOUR_CLOSED) $conditionBuilder->add("thread.isClosed =  ?", [0]);
-				if (!WBB_BOARD_THREAD_TWENTYFOUR_DONE) $conditionBuilder->add("thread.isDone =  ?", [0]);
-				
-				// apply period for threads
-				$conditionBuilder->add("thread.lastPostTime > ?", [TIME_NOW - WBB_BOARD_THREAD_TWENTYFOUR_TIME * 3600]);
-				
-				// apply language filter
-				if (LanguageFactory::getInstance()->multilingualismEnabled() && count(WCF::getUser()->getLanguageIDs())) {
-					$conditionBuilder->add('(thread.languageID IN (?) OR thread.languageID IS NULL)', [WCF::getUser()->getLanguageIDs()]);
-				}
-				
-				$sql = "SELECT	COUNT(*) AS count
-						FROM	wbb".WCF_N."_thread thread
-						".$conditionBuilder;
-				$statement = WCF::getDB()->prepareStatement($sql);
-				$statement->execute($conditionBuilder->getParameters());
-				$row = $statement->fetchArray();
-				self::$twentyfourThreads = $row['count'];
-			}
-		}
-		
-		return self::$twentyfourThreads;
-	}
+class TwentyfourThread extends DatabaseObjectDecorator
+{
+    /**
+     * @inheritDoc
+     */
+    protected static $baseClass = Thread::class;
+
+    /**
+     * number of 24h threads
+     */
+    protected static $twentyfourThreads;
+
+    /**
+     * Returns the number of 24h threads.
+     */
+    public static function getTwentyfourThreads()
+    {
+        if (self::$twentyfourThreads === null) {
+            self::$twentyfourThreads = 0;
+
+            $boardIDs = Board::getAccessibleBoardIDs();
+            // removed ignored boards
+            foreach ($boardIDs as $key => $boardID) {
+                $board = BoardCache::getInstance()->getBoard($boardID);
+                if ($board->isIgnored()) {
+                    unset($boardIDs[$key]);
+                }
+            }
+
+            if (!empty($boardIDs)) {
+                $conditionBuilder = new PreparedStatementConditionBuilder();
+                $conditionBuilder->add("thread.boardID IN (?)", [$boardIDs]);
+                $conditionBuilder->add("thread.isDeleted = 0 AND thread.isDisabled = 0 AND thread.movedThreadID IS NULL");
+
+                // apply options
+                if (!WBB_BOARD_THREAD_TWENTYFOUR_CLOSED) {
+                    $conditionBuilder->add("thread.isClosed =  ?", [0]);
+                }
+                if (!WBB_BOARD_THREAD_TWENTYFOUR_DONE) {
+                    $conditionBuilder->add("thread.isDone =  ?", [0]);
+                }
+
+                // apply period for threads
+                $conditionBuilder->add("thread.lastPostTime > ?", [TIME_NOW - WBB_BOARD_THREAD_TWENTYFOUR_TIME * 3600]);
+
+                // apply language filter
+                if (LanguageFactory::getInstance()->multilingualismEnabled() && \count(WCF::getUser()->getLanguageIDs())) {
+                    $conditionBuilder->add('(thread.languageID IN (?) OR thread.languageID IS NULL)', [WCF::getUser()->getLanguageIDs()]);
+                }
+
+                $sql = "SELECT    COUNT(*) AS count
+                        FROM    wbb" . WCF_N . "_thread thread
+                        " . $conditionBuilder;
+                $statement = WCF::getDB()->prepareStatement($sql);
+                $statement->execute($conditionBuilder->getParameters());
+                $row = $statement->fetchArray();
+                self::$twentyfourThreads = $row['count'];
+            }
+        }
+
+        return self::$twentyfourThreads;
+    }
 }
--- files/lib/page/TwentyfourThreadListPage.class.php
+++ files/lib/page/TwentyfourThreadListPage.class.php
@@ -1,61 +1,82 @@
 <?php
+
+/*
+ * Copyright by Udo Zaydowicz.
+ * Modified by SoftCreatR.dev.
+ *
+ * License: http://opensource.org/licenses/lgpl-license.php
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 namespace wbb\page;
+
 use wbb\data\thread\TwentyfourThreadList;
 use wcf\page\SortablePage;
 use wcf\system\WCF;
 
 /**
  * Shows the list of threads of last 24h.
- *
- * @author		2017-2022 Zaydowicz
- * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @package		com.uz.wbb.twentyfourThreads
  */
-class TwentyfourThreadListPage extends SortablePage {
-	/**
-	 * @inheritDoc
-	 */
-	public $defaultSortField = WBB_BOARD_DEFAULT_SORT_FIELD;
-	
-	/**
-	 * @inheritDoc
-	 */
-	public $defaultSortOrder = WBB_BOARD_DEFAULT_SORT_ORDER;
-	
-	/**
-	 * @inheritDoc
-	 */
-	public $itemsPerPage = WBB_BOARD_THREADS_PER_PAGE;
-	
-	/**
-	 * @inheritDoc
-	 */
-	public $objectListClassName = TwentyfourThreadList::class;
-	
-	/**
-	 * @inheritDoc
-	 */
-	public $validSortFields = ['topic', 'username', 'time', 'views', 'replies', 'lastPostTime', 'cumulativeLikes'];
-	
-	/**
-	 * @inheritDoc
-	 */
-	public function readParameters() {
-		parent::readParameters();
-		
-		if (WCF::getUser()->threadsPerPage) {
-			$this->itemsPerPage = WCF::getUser()->threadsPerPage;
-		}
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	public function assignVariables() {
-		parent::assignVariables();
-		
-		WCF::getTPL()->assign([
-				'validSortFields' => $this->validSortFields
-		]);
-	}
+class TwentyfourThreadListPage extends SortablePage
+{
+    /**
+     * @inheritDoc
+     */
+    public $defaultSortField = WBB_BOARD_DEFAULT_SORT_FIELD;
+
+    /**
+     * @inheritDoc
+     */
+    public $defaultSortOrder = WBB_BOARD_DEFAULT_SORT_ORDER;
+
+    /**
+     * @inheritDoc
+     */
+    public $itemsPerPage = WBB_BOARD_THREADS_PER_PAGE;
+
+    /**
+     * @inheritDoc
+     */
+    public $objectListClassName = TwentyfourThreadList::class;
+
+    /**
+     * @inheritDoc
+     */
+    public $validSortFields = ['topic', 'username', 'time', 'views', 'replies', 'lastPostTime', 'cumulativeLikes'];
+
+    /**
+     * @inheritDoc
+     */
+    public function readParameters()
+    {
+        parent::readParameters();
+
+        if (WCF::getUser()->threadsPerPage) {
+            $this->itemsPerPage = WCF::getUser()->threadsPerPage;
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function assignVariables()
+    {
+        parent::assignVariables();
+
+        WCF::getTPL()->assign([
+            'validSortFields' => $this->validSortFields,
+        ]);
+    }
 }
